558618fef71cdd34a8ee94e1525f4d9b0f237365
amul-mehta <12bce043@nirmauni.ac.in>
2017-03-26 01:56:32 -0400

    intital implementation of webrtc

--- setup/app.js
+++ setup/app.js
@@ -1,17 +1,17 @@
 module.exports = function(app)
 {
-    var connectionString = 'mongodb://127.0.0.1:27017/solicitar';
-
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }
-
-    var mongoose = require("mongoose");
-    // var bcrypt = require("bcrypt");
-    // var SALT_WORK_FACTOR = 10;
-    mongoose.connect(connectionString);
+    // var connectionString = 'mongodb://127.0.0.1:27017/solicitar';
+    //
+    // if(process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
+    //
+    // var mongoose = require("mongoose");
+    // // var bcrypt = require("bcrypt");
+    // // var SALT_WORK_FACTOR = 10;
+    // mongoose.connect(connectionString);
 };
