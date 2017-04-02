f33024c6d82ceb530d85c30d78aa7cfc2ee6f625
Rohan Sapre <sapre.r@husky.neu.edu>
2017-04-02 01:43:51 -0400

    added retrieve availability for users

--- server/services/calendar.service.server.js
+++ server/services/calendar.service.server.js
@@ -2,8 +2,19 @@
  * Created by rohansapre on 4/1/17.
  */
 module.exports = function (app, calendarModel) {
+    app.get("/api/calendar/:userId", getAvailability);
     app.post("/api/calendar/:userId", setAvailability);
     app.put("/api/calendar/:userId", updateAvailability);
+
+    function getAvailability(req, res) {
+        var userId = req.params.userId;
+        calendarModel.getAvailability(userId)
+            .then(function (calendar) {
+                res.json(calendar);
+            }, function (error) {
+                res.sendStatus(500).send(error);
+            })
+    }
 
     function setAvailability(req, res) {
         var userId = req.params.userId;
